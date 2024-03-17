039a33409b0862601699eec6ec7bd0cec10cc3fa
m <139261114+mauriciorfdev@users.noreply.github.com>
2024-03-16 23:20:08 -0300

    delete curly brackets on app.listen()

--- index.js
+++ index.js
@@ -23,6 +23,4 @@ app.get('/', async (req, res)=>{
 })
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
-    console.log('server listen on port ... ', PORT);
-})
+app.listen(PORT, () => console.log('server listen on port ... ', PORT) );
